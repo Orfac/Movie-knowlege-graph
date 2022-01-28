b19da528f2b245100bde6ee3f9b8d8705f4cc91e
Arseniy Molodetskiy <molodeckij.b.2017@gmail.com>
2022-01-28 04:49:40 +0300

    расширил выдачу значений по графу

--- backend/src/main/java/com/itmoproject/backend/model/graph/GraphNode.java
+++ backend/src/main/java/com/itmoproject/backend/model/graph/GraphNode.java
@@ -3,6 +3,7 @@ package com.itmoproject.backend.model.graph;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -10,4 +11,5 @@ public class GraphNode {
     @JsonIgnore
     private String id;
     private String name;
+    private Map<String,Object> properties;
 }
--- backend/src/main/java/com/itmoproject/backend/model/provider/GraphDataManager.java
+++ backend/src/main/java/com/itmoproject/backend/model/provider/GraphDataManager.java
@@ -2,6 +2,7 @@ package com.itmoproject.backend.model.provider;
 
 import com.itmoproject.backend.model.graph.GraphNode;
 import com.itmoproject.backend.model.graph.GraphRelation;
+import org.neo4j.driver.internal.InternalNode;
 import org.springframework.data.neo4j.core.Neo4jClient;
 import org.springframework.stereotype.Service;
 
@@ -14,39 +15,44 @@ import java.util.stream.Collectors;
  */
 @Service
 public class GraphDataManager {
-    private final Neo4jClient client;
+	private final Neo4jClient client;
 
-    public GraphDataManager(Neo4jClient client) {
-        this.client = client;
-    }
+	public GraphDataManager(Neo4jClient client) {
+		this.client = client;
+	}
 
-    public Map<String, GraphNode> getAllNodes() {
-        String query = "MATCH (p) RETURN p.id as id, p.name as name";
+	public Map<String, GraphNode> getAllNodes() {
+		String query = "MATCH (p) RETURN (p)";
 
-        return client.query(query)
-                .fetchAs(GraphNode.class)
-                .mappedBy((typeSystem, record) -> new GraphNode(
-                        record.get("id").asString(),
-                        record.get("name").asString()
-                )).all()
-                .stream()
-                .collect(Collectors.toMap(GraphNode::getId, (node) -> node));
-    }
+		return client.query(query)
+			.fetchAs(GraphNode.class)
+			.mappedBy((typeSystem, record) -> {
+					var node = ((InternalNode) record.asMap().get("p"));
+					return new GraphNode(
+						node.get("id").asString(),
+						node.get("name").asString(),
+						node.asMap());
+				}
+			).all()
+			.stream()
+			.collect(Collectors.toMap(GraphNode::getId, (node) -> node));
+	}
 
-    public Map<String, GraphRelation> getAllRelations() {
-        String query = "MATCH ()-[r]->() RETURN startNode(r).id as source, endNode(r).id as target, type(r) as name";
+	public Map<String, GraphRelation> getAllRelations() {
+		String query
+			= "MATCH ()-[r]->() RETURN startNode(r).id as source, endNode(r).id as target, type(r) as name";
 
-        return client.query(query)
-                .fetchAs(GraphRelation.class)
-                .mappedBy((typeSystem, record) -> new GraphRelation(
-                        record.get("name").asString(),
-                        record.get("source").asString(),
-                        record.get("target").asString()
-                )).all()
-                .stream()
-                .collect(Collectors.toMap(
-                        rel -> UUID.randomUUID().toString(),
-                        rel -> rel)
-                );
-    }
+		return client.query(query)
+			.fetchAs(GraphRelation.class)
+			.mappedBy((typeSystem, record) -> new GraphRelation(
+				record.get("name").asString(),
+				record.get("source").asString(),
+				record.get("target").asString()
+			)).all()
+			.stream()
+			.collect(Collectors.toMap(
+				rel -> UUID.randomUUID().toString(),
+				rel -> rel)
+			);
+	}
 }
